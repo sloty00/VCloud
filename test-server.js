cef6297cc70f6256e60a4ae6878f4bf0ae230802
sloty00 <jvoyarzun81@gmail.com>
2026-06-06 13:28:37 +0000

    Deploying to gh-pages from @ sloty00/VCloud@68fee333c53f2742d970361bb7e5afd4b77dafab 🚀

--- test-server.js
+++
@@ -1,9 +0,0 @@
-// backend/test-server.js
-const app = require('./src/index.js'); // Ajustado a la ruta correcta
-const http = require('http');
-
-const server = http.createServer(app);
-server.listen(0, () => {
-  console.log('Servidor iniciado correctamente para el test');
-  process.exit(0);
-});
